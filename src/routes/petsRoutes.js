a0ded05f29833e4c7335899878f5dc904dcf234d
Arnascode <vilkoncius7@gmail.com>
2022-05-04 13:02:08 +0300

    Co-authored-by: Kkestutis <Kkestutis@users.noreply.github.com>

--- src/routes/petsRoutes.js
+++ src/routes/petsRoutes.js
@@ -19,4 +19,37 @@ petsRoutes.get('/pets', async (req, res) => {
   }
 });
 
+petsRoutes.post('/pets', async (req, res) => {
+  let conn;
+  try {
+    const newPostObj = req.body;
+    console.log('newPostObj ===', newPostObj);
+
+    // eslint-disable-next-line object-curly-newline
+    const { name, dob, client_email, archived } = newPostObj;
+
+    conn = await mysql.createConnection(dbConfig);
+    const sql = `
+    INSERT INTO pets (name, dob, client_email, archived)
+    VALUES (?, ?, ?, ?)
+    `;
+    const [insertResultObj] = await conn.execute(sql, [
+      name,
+      dob,
+      client_email,
+      archived,
+    ]);
+    if (insertResultObj.affectedRows === 1) {
+      res.status(201).json(insertResultObj);
+      return;
+    }
+    throw new Error('affected row not 1');
+  } catch (error) {
+    console.log('error creating pets', error);
+    res.sendStatus(500);
+  } finally {
+    await conn?.end();
+  }
+});
+
 module.exports = petsRoutes;
